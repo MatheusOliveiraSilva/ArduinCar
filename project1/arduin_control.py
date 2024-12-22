a1a334103153f6a2c9fbd684d4e8796f719e6c4a
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2024-12-22 16:08:09 -0300

    arduin control python provisory code

---
+++ project1/arduin_control.py
@@ -0,0 +1,18 @@
+import serial
+import time
+
+# Configuração da porta serial
+arduino = serial.Serial(port='/dev/cu.usbmodemXXXX', baudrate=9600, timeout=1)
+time.sleep(2)  # Aguarde 2 segundos para inicializar a comunicação
+
+def led_control(state):
+    """Envia comando para ligar/desligar o LED"""
+    arduino.write(state.encode())
+
+    comando = ""
+    if state == '1':
+        comando = "LED ligado"
+    elif state == '0':
+        comando = "LED desligado"
+
+    return f"Comando enviado: {comando}"
